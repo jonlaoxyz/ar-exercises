523a11a95008f8604b6933581d1a56d57c0584d4
Jonathan Lao <jon.lao@gmail.com>
2024-01-24 08:53:46 -0600

    completed exercise_2

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,14 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+
+@store1 = Store.find(1)
+@store2 = Store.find(2)
+
+puts "Name of @store1: #{@store1.name}"
+puts "Name of @store2: #{@store2.name}"
+
+new_name = "Calgary"
+@store1.update(name: new_name)
+
+puts "Updated name of @store1: #{@store1.name}"
