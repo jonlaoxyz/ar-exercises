669223f98d02aa42add0704f0cc80e9bd4a59b9f
Jonathan Lao <jon.lao@gmail.com>
2024-01-24 13:29:27 -0600

    implement exercise_7 requirements and validations

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,15 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+puts "Please enter a store name:"
+store_name = gets.chomp
+
+new_store = Store.new(name: store_name)
+if new_store.save
+  puts "Success! Store created!"
+else
+  puts "Failed to create store! Errors:"
+  new_store.errors.full_messages.each do |error_message|
+    puts error_message
+  end
+end
