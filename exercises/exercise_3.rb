88ea43312782cac75570f690979819beec7ef2e0
Jonathan Lao <jon.lao@gmail.com>
2024-01-24 08:59:42 -0600

    completed exercise_3

--- exercises/exercise_3.rb
+++ exercises/exercise_3.rb
@@ -6,3 +6,13 @@ puts "Exercise 3"
 puts "----------"
 
 # Your code goes here ...
+
+@store3 = Store.find(3)
+
+puts "Current number of stores: #{Store.count}"
+
+@store3.destroy
+
+puts "New number of stores: #{Store.count}"
+
+
